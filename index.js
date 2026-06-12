7badfd0425ea1b04198f61ef216ffbad784f45cc
Mark <149008372+markduplock@users.noreply.github.com>
2026-06-12 21:49:42 +0100

    the greatest CRUD app of all time

--- index.js
+++ index.js
@@ -9,14 +9,40 @@ function addItem() {
     alert("Input cannot be empty");
   } else {
     const newListItem = document.createElement("li");
+    const newCheckBox = document.createElement("input");
+    const newDeleteButton = document.createElement("button");
+
+    newCheckBox.type = "checkbox";
     newListItem.textContent = itemText;
+    newDeleteButton.textContent = "Delete Task";
+
     list.appendChild(newListItem);
+    newListItem.appendChild(newCheckBox);
+    newListItem.appendChild(newDeleteButton);
+    checkItem(newCheckBox, newListItem);
+    deleteItem(newDeleteButton, newListItem);
   }
   inputField.value = "";
   inputField.focus();
 }
 
-addItemForm.addEventListener("submit", function (event) {
+function checkItem(newCheckBox, newListItem) {
+  newCheckBox.addEventListener("change", (event) => {
+    if (event.target.checked) {
+      newListItem.style.textDecoration = "line-through";
+    } else {
+      newListItem.style.textDecoration = "none";
+    }
+  });
+}
+
+function deleteItem(newDeleteButton, newListItem) {
+  newDeleteButton.addEventListener("click", () => {
+    newListItem.remove();
+  });
+}
+
+addItemForm.addEventListener("submit", (event) => {
   event.preventDefault();
   addItem();
 });

13239fc2707e39d3f7e29ede84eece21678587da
Mark <149008372+markduplock@users.noreply.github.com>
2026-06-28 14:08:55 +0100

    Bulk delete count working

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const inputField = document.getElementById("add-list-item");
 const list = document.getElementById("list");
 const addItemForm = document.getElementById("add-item-form");
-const deleteSection = document.getElementById("delete-selected-button");
-let selectionCount = 0;
+const deleteSelection = document.getElementById("delete-selected-button");
+const selectionCount = document.getElementById("selection-count");
+let selectionCountContent = 0;
+selectionCount.textContent = selectionCountContent;
 
 function addItem() {
   const itemText = inputField.value.trim();
@@ -22,44 +24,53 @@ function addItem() {
     newListItem.append(newCheckBox);
     newListItem.append(itemText);
     newListItem.append(newDeleteButton);
-    checkItem(newCheckBox, newListItem);
-    deleteItem(newDeleteButton, newListItem);
+    checkItem(newCheckBox);
+    deleteItem(newDeleteButton, newListItem, newCheckBox);
   }
   inputField.value = "";
   inputField.focus();
 }
 
-function checkItem(checkBox, listItem) {
+function checkItem(checkBox) {
   checkBox.addEventListener("change", (event) => {
     if (checkBox.checked) {
-      selectionCount++;
-      if (selectionCount > 0) {
-        deleteSection.disabled = false;
+      selectionCountContent++;
+      if (selectionCountContent > 0) {
+        deleteSelection.disabled = false;
       }
     } else {
-      selectionCount--;
-      if (selectionCount < 1) {
-        deleteSection.disabled = true;
+      selectionCountContent--;
+      if (selectionCountContent < 1) {
+        deleteSelection.disabled = true;
       }
     }
-    console.log(selectionCount);
+    selectionCount.textContent = selectionCountContent;
+    // ** DEBUG **
+    //console.log(selectionCountText);
   });
 }
 
-function deleteItem(newDeleteButton, newListItem) {
-  newDeleteButton.addEventListener("click", () => {
-    newListItem.remove();
+function deleteItem(deleteButton, listItem, checkbox) {
+  deleteButton.addEventListener("click", () => {
+    if (checkbox.checked) {
+      selectionCountContent--;
+      if (selectionCountContent < 1) {
+        deleteSelection.disabled = true;
+      }
+      selectionCount.textContent = selectionCountContent;
+    }
+    listItem.remove();
+    // ** DEBUG **
+    //console.log(selectionCountText);
   });
 }
-
-function deleteSelection() {}
 
 addItemForm.addEventListener("submit", (event) => {
   event.preventDefault();
   addItem();
 });
 
-deleteSection.addEventListener("click", (event) => {
+deleteSelection.addEventListener("click", (event) => {
   event.preventDefault();
-  deleteSelection();
+  //deleteSelection();
 });
